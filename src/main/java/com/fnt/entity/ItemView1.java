142962084bb36d410fe1654beca0375c7906ebe9
Thomas Fanto <thomas@fanto.se>
2018-07-12 12:25:32 +0200

    getter setters etc

--- src/main/java/com/fnt/entity/ItemView1.java
+++ src/main/java/com/fnt/entity/ItemView1.java
@@ -36,4 +36,97 @@ public class ItemView1 {
 		this.price = price;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getItemnumber() {
+		return itemnumber;
+	}
+
+	public void setItemnumber(String itemnumber) {
+		this.itemnumber = itemnumber;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getInStock() {
+		return inStock;
+	}
+
+	public void setInStock(Integer inStock) {
+		this.inStock = inStock;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((inStock == null) ? 0 : inStock.hashCode());
+		result = prime * result + ((itemnumber == null) ? 0 : itemnumber.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemView1 other = (ItemView1) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (inStock == null) {
+			if (other.inStock != null)
+				return false;
+		} else if (!inStock.equals(other.inStock))
+			return false;
+		if (itemnumber == null) {
+			if (other.itemnumber != null)
+				return false;
+		} else if (!itemnumber.equals(other.itemnumber))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
 }
